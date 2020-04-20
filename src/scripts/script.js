2c101777ce9baccc333cc86597295d6fa3f97c4a
AlexandrWilliams <canablak@gmail.com>
2020-04-19 23:38:34 -0400

    start work with server

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -15,13 +15,15 @@ const popUpSignInHtml = `
 			<div class="popUp__conteiner">
 				<img class="popUp__close-icon" src="${jumpToFile}" alt="close icon">
 				<h3 class="popUp__title" >Вход</h3>
-				<form class="popUp__login-form" name="logIn" action="post" method="POST">
+				<form novalidate class="popUp__login-form" name="signIn" action="https://api.alexanderwilliams.us/signin" method="POST">
 					<label class="popUp__label popUp__label_email" for="email">Email</label>
 					<input class="popUp__input" type="email" name="email" minlength="6" maxlength="30" placeholder="Введите почту" required>
+					<span id="error-email" class="error-sms error-sms__name"></span>
 					<span class="popUp__input-line"></span>
 					<label class="popUp__label popUp__label_password" for="password">Пароль</label>
-					<input class="popUp__input" type="password" name="password" minlength="6" maxlength="16" placeholder="Введите пароль" required>
+					<input class="popUp__input" type="password" name="password" minlength="8" maxlength="16" placeholder="Введите пароль" required>
 					<span class="popUp__input-line"></span>
+					<span id="error-password" class="error-sms error-sms__name"></span>
 					<input class="popUp__submit" type="submit" name="submit" value="Войти">
 					<h4 class="popUp__under-button-txt">или <span class="popUp__signup">Зарегистрироваться</span></h4>
 				</form>
@@ -30,16 +32,19 @@ const popUpSignUpHtml = `
 			<div class="popUp__conteiner">
 				<img class="popUp__close-icon" src="${jumpToFile}" alt="close icon">
 				<h3 class="popUp__title" >Регистрация</h3>
-				<form class="popUp__login-form" name="logIn" action="post" method="POST">
+				<form novalidate class="popUp__login-form" name="signUp" action="https://api.alexanderwilliams.us/signup" method="POST">
 					<label class="popUp__label popUp__label_email" for="email">Email</label>
 					<input class="popUp__input" type="email" name="email" minlength="6" maxlength="30" placeholder="Введите почту" required>
 					<span class="popUp__input-line"></span>
+					<span id="error-email" class="error-sms error-sms__name"></span>
 					<label class="popUp__label popUp__label_password" for="password">Пароль</label>
-					<input class="popUp__input" type="password" name="password" minlength="6" maxlength="16" placeholder="Введите пароль" required>
+					<input class="popUp__input" type="password" name="password" minlength="8" maxlength="16" placeholder="Введите пароль" required>
 					<span class="popUp__input-line"></span>
+					<span id="error-password" class="error-sms error-sms__name"></span>
 					<label class="popUp__label popUp__label_name" for="name">Имя</label>
-					<input class="popUp__input" type="text" name="name" minlength="6" maxlength="16" placeholder="Введите свое имя">
+					<input class="popUp__input" type="text" name="name" minlength="4" maxlength="16" placeholder="Введите свое имя">
 					<span class="popUp__input-line"></span>
+					<span id="error-name" class="error-sms error-sms__name"></span>
 					<input class="popUp__submit popUp__submit_signUp" type="submit" name="submit" value="Зарегистрироваться">
 					<h4 class="popUp__under-button-txt">или <span class="popUp__signin">Войти</span></h4>
 				</form>
@@ -56,11 +61,12 @@ logInButton.addEventListener('click', (e)=>{
 	pageConteiner.querySelector('.popUp').insertAdjacentHTML('beforeend', popUpSignInHtml);
 	popUp();
 })
-function popUp() {
+function popUp(signUpTrue) {
 	const popUpSection = document.querySelector('.popUp');
 	let popUpConteiner = popUpSection.querySelector('.popUp__conteiner');
 	let popUpSubmitSignUp;
 	let popUpSuccessefull;
+	preValidator('signIn');
 	popUpSection.addEventListener('click', (e) => {
 		const eClass = e.target.className;
 		(eClass === 'popUp__background')?popUpSection.remove():false;
@@ -76,6 +82,7 @@ function popUp() {
 		if(eClass === 'popUp__signup') {
 			popUpConteiner.remove();
 			popUpSection.insertAdjacentHTML('beforeend', popUpSignUpHtml);
+			preValidator('signUp');
 			popUpConteiner = popUpSection.querySelector('.popUp__conteiner');
 			popUpSubmitSignUp = popUpConteiner.querySelector('.popUp__submit_signUp');
 		};
@@ -83,8 +90,9 @@ function popUp() {
 			popUpConteiner.remove();
 			popUpSection.insertAdjacentHTML('beforeend', popUpSignInHtml);
 			popUpConteiner = popUpSection.querySelector('.popUp__conteiner');
+			preValidator('signIn');
 		}
-		if(eClass === 'popUp__submit_signUp') {
+		if(eClass === 'popUp__submit_signUp' || signUpTrue === true) {
 			//server return successefull response
 			popUpConteiner.remove();
 			popUpSection.insertAdjacentHTML('beforeend', popUpSignUpSuccessefull);
@@ -142,4 +150,168 @@ function whiteScheme() {
 		exitImg.classList.toggle('header__button_exit-icon_black');
 	}
 }
+//authorization
+function preValidator(formType){
+	let form
+	if (formType === "signIn"){
+		form = document.forms.signIn;
+	} else if(formType === "signUp"){
+		form = document.forms.signUp;
+	}
+	form.addEventListener('input', (e)=>{
+		if (e.name !== 'submit') {
+			profileEditorHandler(form)
+		}
+	})
+	form.addEventListener('submit', (e)=>{
+		e.preventDefault();
+	})
+}
+
+const langRu = { 
+	  validationLenghtEmail: 'Email быть от 6 до 30 символов, и содержать @',
+	  validationLenghtPassword: 'Должно быть от 8 до 16 символов',
+	  validationLenghtName: 'Имя должно содержать минимум 4 символа',
+	  required: 'Это обязательное поле'
+}
+
+function validator (e) {
+	  const error = document.querySelector(`#error-${e.name}`);
+	  let submitButton = document.querySelector('.popUp__submit');
+	  if (!e.checkValidity()) {
+	      if (e.value.length === 0) {
+	        error.textContent = langRu.required;
+	      } else if (e.name === 'email') {
+	        error.textContent = langRu.validationLenghtEmail;
+	      } else if (e.name === 'password') {
+	        error.textContent = langRu.validationLenghtPassword;
+	      } else if (e.name === 'name') {
+	        error.textContent = langRu.validationLenghtName;
+	      }
+	      submitButton.classList.remove('popUp__submit_active')
+	      return false;
+	  }
+
+	  error.textContent = '';
+	  return true;
+};
+
+function profileEditorHandler(e) {
+	let submitButton = document.querySelector('.popUp__submit');
+
+    event.preventDefault();
+
+    const inputs = Array.from(e.elements);    
+   
+    let valid = true;
+
+    inputs.forEach((e) => {
+    	console.log(e)
+      if (e.name !== 'submit') {
+        if (!validator(e)) { 
+          valid = false;
+        }
+      } 
+    });
+
+    if (valid) {
+    	submitButton.classList.add('popUp__submit_active');
+	    e.addEventListener('submit', (elem)=>{
+			sendReq(e);
+			e.reset();
+			submitButton.classList.remove('popUp__submit_active')
+		})
+    }
+}
+function sendReq(form){
+	let email;
+	let password;
+	let name;
+	form.elements.forEach((e)=>{
+		(e.name === 'email')?email = e.value:false;
+		(e.name === 'password')?password = e.value:false;
+		(e.name === 'name')?name = e.value:false;
+	})
+	console.log(email, password, name)
+	if (name === undefined) {
+		fetch("https://api.alexanderwilliams.us/signin", {
+			  mode : 'no-cors',
+		      method: 'POST',
+		      headers: {
+		      	'Access-Control-Allow-Origin': '*',
+			    'Content-Type': 'application/json'
+			  },
+		      body: JSON.stringify({
+		        "email": email,
+		        "password": password
+		      }),
+		      redirect: 'follow'
+		    }
+	    )
+	    .then((res) => {
+	        if (res.ok) {
+	        	console.log(true)
+	          return res.json();
+	        } return Promise.reject(`Error:${res.status}`);
+      	})
+	    .catch(error => console.log('error', error));
+	} else if (name) {
+		fetch(`https://api.alexanderwilliams.us/signup`, {
+		  mode : 'no-cors',
+	      method: 'POST',
+	      headers: {
+	      	'Access-Control-Allow-Origin': '*',
+	        'Content-Type': 'application/json'
+	      }, 
+	      body: JSON.stringify({
+	        email: email,
+	        password: password,
+	        name: name,
+	      }),
+		  redirect: 'follow'
+	    })
+	    .then((res) => {
+        if (res.ok) {
+        	popUp(true)
+        	console.log(true)
+          return res.json();
+        } console.log(false)
+        return Promise.reject(`Error:${res.status}`);
+      });
+	}
+}
 /// обработка после атворизации
+//API Key 3c48624ca3ff47eb9d50ce64b52e3f1e
+const formSearch = document.forms.search;
+formSearch.addEventListener('submit', (e) => {
+	event.preventDefault();
+	let article;
+	let inputs = formSearch.elements;
+	inputs.forEach((e)=>{
+		(e.name === 'search')?article = e.value:false;
+	})
+	let date;
+	const url = `https://newsapi.org/v2/everything?q=${article}&from=2020-04-07&sortBy=popularity&apiKey=3c48624ca3ff47eb9d50ce64b52e3f1e`;
+	const req = new Request(url);
+	fetch(req)
+	    .then((res) => {
+	    	console.log(res.json());
+	        if (res.ok) {
+	          return res.json();
+	        } return Promise.reject(`Error:${res.status}`);
+      	})
+	    .catch(error => console.log('error', error));
+})
+//https://newsapi.org/v2/everything?q=bitcoin&apiKey=3c48624ca3ff47eb9d50ce64b52e3f1e
+// var url = 'http://newsapi.org/v2/everything?' +
+//           'q=Apple&' +
+//           'from=2020-04-14&' +
+//           'sortBy=popularity&' +
+//           'apiKey=3c48624ca3ff47eb9d50ce64b52e3f1e';
+
+// var req = new Request(url);
+
+// fetch(req)
+//     .then(function(response) {
+//         console.log(response.json());
+//     })
